1ecea72c938b9908b01100a8540a557a3180f156
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2017-01-28 15:38:19 -0500

    buttonBuzz.ino: Replacement of the Tone library by NewTone

--- buttonBuzz/buttonBuzz.ino
+++ buttonBuzz/buttonBuzz.ino
@@ -1,4 +1,5 @@
-#include "pitches.h" // Permet d'utiliser les noms des notes (ex.: NOTE_A4) plutot que la fréquence
+#include "/usr/share/doc/arduino-core/examples/02.Digital/toneKeyboard/pitches.h" // Permet d'utiliser les noms des notes (ex.: NOTE_A4) plutot que la fréquence
+#include <NewTone.h> // Librairie qui permet de commander au buzzer
 
 const int buzzerPin = 8; // La broche connectée au signal du buzzer 
 const int buttonPin = 7; // La broche connectée au bouton du joystick
@@ -13,7 +14,7 @@ void loop()
 {
   while (digitalRead(buttonPin) == LOW)
   {
-    tone(buzzerPin, NOTE_A4, 20);
+    NewTone(buzzerPin, NOTE_A4, 20);
   }
   
 }
